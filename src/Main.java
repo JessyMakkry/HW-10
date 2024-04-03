1a4ae15986ff7cc791045f3cd417e65833996712
JessyMakkry <jessy.makkry@bk.ru>
2024-04-03 20:58:13 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,14 @@ public class Main {
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
-        String fullName = lastName+' '+firstName+' '+middleName;
-        System.out.println("Ф.И.О. сотрудника - "+fullName);
+        String fullName = lastName + ' ' + firstName + ' ' + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
         System.out.println("Task 2");
-
+        String taskName = fullName.toUpperCase();
+        System.out.println("Ф.И.О. сотрудника для заполнения отчёта - " + taskName);
+        System.out.println("Task 3");
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace('ё', 'е');
+        System.out.println("Данные Ф.И.О. сотрудника - " + fullName);
     }
 }
